6a6ee9cca867000a7e557c739ce3ad484e830082
jo-dev123 <johnsondegbor@gmail.com>
2022-03-14 16:04:15 -0700

    adding 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <nbistd.h>
+/**
+ *  main - Entry Point
+ *  @void -no parameter
+ *
+ *  Return: Given strin
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+			sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+	return (1);
+}
