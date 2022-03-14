0adfd5d2c46b549399b8ca281bc5af5530d2a601
jo-dev123 <johnsondegbor@gmail.com>
2022-03-13 22:01:05 -0700

    adding 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @void: no parameter
+ *
+ * Description: prints string
+ * Return: 0 if true
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+
+	return (0);
+}
