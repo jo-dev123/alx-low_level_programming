25934ce79ffd877679aff2c6c2963540c5e583a2
jo-dev123 <johnsondegbor@gmail.com>
2022-03-14 10:18:19 -0700

    adding 6-size.c replacement

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - entry point
- * @void: no parameter
+ * main - Entry Point
+ * @void: no parameters available
  *
- * Description: accept %d as integer
- * Return: 0 if true
+ * Description: printing size of each data type
+ * Return; 0 if true
  */
 
 int main(void)
@@ -14,7 +14,7 @@ int main(void)
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %d bytes(s)\n", sizeof(float));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 
 	return (0);
 }
