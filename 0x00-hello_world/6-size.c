0c6bc0ab1333d9f23de237a29d7572058bf9b428
jo-dev123 <johnsondegbor@gmail.com>
2022-03-13 23:15:02 -0700

    adding 'byte(s)' to string

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@
 
 int main(void)
 {
-	printf("Size of a char: %d\n", sizeof(char));
-	printf("Size of an int: %d\n", sizeof(int));
-	printf("Size of a long long int: %d\n", sizeof(long long int));
-	printf("Size of a float: %d\n", sizeof(float));
+	printf("Size of a char: %d\n byte(s)", sizeof(char));
+	printf("Size of an int: %d\n byte(s)", sizeof(int));
+	printf("Size of a long long int: %d\n byte(s)", sizeof(long long int));
+	printf("Size of a float: %d\n bytes(s)", sizeof(float));
 
 	return (0);
 }
