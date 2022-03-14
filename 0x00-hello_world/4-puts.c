d3f6eb736f4d4d1be2983f9e2e9e822a5f531ac9
jo-dev123 <johnsondegbor@gmail.com>
2022-03-13 20:11:52 -0700

    Adding 4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @void: no parameter
+ *
+ * Description: puts string to standard output
+ * Retur: 0 if true
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingualpuzzle");
+
+	return (0);
+}
