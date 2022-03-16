e2599209e3c34379fb68086052d8a91f68bf72af
jo-dev123 <johnsondegbor@gmail.com>
2022-03-16 15:15:03 -0700

    meeting standard requirement

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,14 +10,14 @@
  * Return: 0 if true
  */
 
-int main()
+int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("%s %d is %d and is", last,n, n % 10);
-	if(n % 10 > 5)
+	printf("%s %d is %d and is", last, n, n % 10);
+	if (n % 10 > 5)
 	{
 		printf("greater than 5\n");
 	}
