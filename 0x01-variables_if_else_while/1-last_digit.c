00ab57a76a8ab4c1ba290dc86b49dbd70be63341
jo-dev123 <johnsondegbor@gmail.com>
2022-03-17 09:40:10 -0700

    adding semicolon

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,7 +13,7 @@
 int main(void)
 {
 	int n;
-	char last[] = "Last digit of"
+	char last[] = "Last digit of";
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
@@ -28,7 +28,7 @@ int main(void)
 	}
 	else
 	{
-		printf("less than 5 and not 0\n");
+		printf("less than 6 and not 0\n");
 	}
 	return (0);
 }
