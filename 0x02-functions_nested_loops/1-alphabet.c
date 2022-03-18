c319723f4e3945ea9a45044cbd0691b3cdbd0125
jo-dev123 <johnsondegbor@gmail.com>
2022-03-18 12:28:17 -0700

    adding 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h";
+
+/**
+ * main- Entry Point
+ * @void: no Parameter
+ *
+ * Description: print the alphabet
+ * Return: 0 if True
+ */
+
+
+int main(void)
+{
+	for (char st = 'a'; st <= 'z'; st++)
+	{
+		_putchar(st);
+	}
+
+	_putchar("\n");
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,2 @@
 int _putchar(char c);
+void print_alpha(void);
