7d1f4062ad3aa0a06418f75b5375fdef48c8e7c4
jo-dev123 <johnsondegbor@gmail.com>
2022-03-18 14:13:53 -0700

    adding 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ * @void: no parameter
+ *
+ * Description: a-z 10x
+ * Return: 0 if successful
+ */
+
+void print_alphabet_x10(void)
+{
+	char fl;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (fl = 'a'; fl <= 'z'; i++)
+		{
+			_putchar(fl);
+		}
+		_putchar("\n");
+	}
+}
