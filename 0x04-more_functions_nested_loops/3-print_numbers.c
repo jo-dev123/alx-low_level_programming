4f2f3b17b4750460a23b2175fa60c72fd531ae00
jo-dev123 <johnsondegbor@gmail.com>
2022-03-18 20:16:23 -0700

    adding 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - Printing 0-9
+ * @void: no parameter
+ *
+ * Description: Print int from 0-9 then a 
+ * newline
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int st;
+
+	for (st = 0; st <= 9; st++)
+		_putchar(st);
+	_putchar('\n');
+}
