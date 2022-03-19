f90ebc506f06ab630e13d013ceba5820e13f3445
jo-dev123 <johnsondegbor@gmail.com>
2022-03-18 17:25:39 -0700

    adding 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper - Entry Point
+ * @c: stores letter for checking
+ *
+ * Description: find uppercases
+ * Return: 0 if successful
+ */
+
+int _isupper(int c)
+{
+	if (c < 'a')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
