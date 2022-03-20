e0607d84838cafa62d60c724f60f932f27393de7
jo-dev123 <johnsondegbor@gmail.com>
2022-03-20 16:09:36 -0700

    adding 6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * @void: no parameter
+ *
+ * Description: print 1-9 decimal
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i);
+	}
+
+	putchar("\n");
+	return (0);
+}
