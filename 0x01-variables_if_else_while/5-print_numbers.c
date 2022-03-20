ef4bbac7702f4b22225a4b3e5a6182443bbc2dff
jo-dev123 <johnsondegbor@gmail.com>
2022-03-20 15:43:21 -0700

    adding the statement and declaration for loop

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * @void: no parameter
+ *
+ * Description: print 1-9 not binary
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d\n", i);
+	}
+}
