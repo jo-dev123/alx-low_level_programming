67fae4b0f633eb0a046f7a69e8926c5cb1a49d88
jo-dev123 <johnsondegbor@gmail.com>
2022-03-25 14:27:03 -0700

    adding 6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/5-strings_toupper.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * string_toupper - lower to uppercase
+ * @string: string parameter
+ *
+ * Return: string
+ */
+
+char *cap_string(char *str)
+{
+	int i;
+	int j;
+	char c[] = {44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 32, 10, 9};
+
+	i = 0;
+
+	while ()
+	{
+		if (i == 0 && str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+
+		j = 0;
+		while (c[j] != '\0')
+		{
+			if (c[j] == str[i] && (str[i + 1] >= 97 && str[i + 1] <= 122))
+			{
+				str[i + 1] = str[i + 1] - 32;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (str);
+}
