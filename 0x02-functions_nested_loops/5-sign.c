acba4332f1433f92cbb026612b8493df126eff5e
jo-dev123 <johnsondegbor@gmail.com>
2022-03-26 11:44:20 -0700

    adding 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - print signs depend on n
+ * @n: int parameter
+ *
+ * Return: a sign + || -
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('-');
+		return (0);
+	}
+	else
+	{
+		_putchar('-1');
+		return (-1);
+	}
+}
