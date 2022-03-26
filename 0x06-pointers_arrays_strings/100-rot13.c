35ade67d4f81f7ee1be6605c6793c02d3701e42a
jo-dev123 <johnsondegbor@gmail.com>
2022-03-25 20:26:41 -0700

    adding 100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rot13 - Caesers Ciphering
+ * @str: string parameter
+ *
+ * Return: Weakly encrypted str
+ */
+
+char *rot13(char *str)
+{
+	int i,c;
+	char realLet = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+	char cphLet = "NnOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (c = 0; realLet[c] != '\0'; c++)
+		{
+			if (realLet[c] == str[i])
+				str[i] = cphLet[c];
+		}
+	}
+	return (str);
+}
