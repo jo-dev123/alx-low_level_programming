5bbc5320f585fc49a0fd953b7d733fa71ed1a3f3
jo-dev123 <johnsondegbor@gmail.com>
2022-03-25 21:17:42 -0700

    new algorithm 101-print_numbers.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,7 +8,16 @@
 
 void print_number(int n)
 {
-	_putchar(n);
+	unsigned int i = n;
 
-	return (n);
+	if (n < 0)
+	{
+		_putchar(45);
+		i = -1;
+	}
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar(i % 10 + '0');
 }
