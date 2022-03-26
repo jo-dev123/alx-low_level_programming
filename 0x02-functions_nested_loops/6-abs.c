db4cfe2ee0c93a35720dba2510dd92f1aa0b5041
jo-dev123 <johnsondegbor@gmail.com>
2022-03-26 12:25:50 -0700

    adding 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - prints absolute value
+ * @n: parameter of int
+ *
+ * Return: return 0;
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		return (n * 1);
+	return (n);
+}
