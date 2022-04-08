a93cf02c8213e37eb31ede59ebfb6bb698829ef3
jo-dev123 <johnsondegbor@gmail.com>
2022-04-08 08:53:16 -0700

    Adding 0-whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,9 +9,9 @@
  * Return: name of program
  */
 
-int main(__attribute__((unused)) int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	printf("%s\n", argv[argc * 0]);
 
 	return (0);
 }
