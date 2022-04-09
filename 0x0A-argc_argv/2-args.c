d32b06d6220bc41984ddc2cdc6a0a88d8eabbdac
jo-dev123 <johnsondegbor@gmail.com>
2022-04-09 05:50:40 -0700

    adding 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * @argc: counting argument
+ * @argv: array of pointer to string
+ *
+ * Description: Prints all arguments including
+ * the program itself
+ * Return: arguments
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc && i >= 0; i++)
+	{
+		printf("%s\n",argv[i]);
+	}
+
+	return (0);
+}
