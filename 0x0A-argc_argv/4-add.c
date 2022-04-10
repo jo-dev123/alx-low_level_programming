300288617781e85956dd0ab4aaac3b3dd83c2c35
jo-dev123 <johnsondegbor@gmail.com>
2022-04-10 06:11:48 -0700

    adding 4-adds

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * main - Entry point 
+ * @argc: counts program argument
+ * @argv: Array of Pointers to string
+ *
+ * Return: 0 if successful, else 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, len, ad;
+	char *ptr;
+
+	if (argc < 2)
+		printf("0\n");
+	else
+	{
+		ad = 0;
+		for (i = 1; i < argc; i++)
+		{
+			ptr = argv[i];
+			len = strlen(ptr);
+
+			for (j = 1; j < length; j++)
+			{
+				if (isdigit(*(ptr + j)) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum += atoi(argv[i]);
+		}
+	}
+}
