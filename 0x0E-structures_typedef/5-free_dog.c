a8f809efdfdea9733df83138dc33471d96514791
jo-dev123 <johnsondegbor@gmail.com>
2022-04-11 22:47:26 -0700

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdib.h>
+#include "dog.h"
+
+/**
+ * free_dog - initialize a variable of type struct dog
+ * @d:dog's detail
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d-owner);
+		free(d);
+	}
+}
