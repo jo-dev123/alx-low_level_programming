a544ccf0e964af89783a11122b60fe2e832efbff
jo-dev123 <johnsondegbor@gmail.com>
2022-04-11 20:13:43 -0700

    adding 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @void: mo parameter
+ *
+ * Return: 0 if successful;
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
