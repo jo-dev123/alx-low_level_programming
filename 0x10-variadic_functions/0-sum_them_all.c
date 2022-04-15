6a389f99a595c5bbcc71e77b9c4eba3a919274e2
jo-dev123 <johnsondegbor@gmail.com>
2022-04-14 22:31:43 -0700

    dajusting header

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,16 +9,15 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int i;
-	unsigned int sum = 0;
 	va_list argn;
+	unsigned int sum = 0, i;
+
+	va_start(argn, n);
 
 	if (n == 0)
 	{
 		return (0);
 	}
-
-	va_start(argn, n);
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(argn, int);
