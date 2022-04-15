9fff3f51fb5d2385200c4727f56769fbc5c3c814
jo-dev123 <johnsondegbor@gmail.com>
2022-04-14 22:38:11 -0700

    similar header

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,7 +10,7 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list list;
-	unsigned int sum = 0, i;
+	unsigned int i, sum = 0;
 
 	va_start(list, n);
 
@@ -20,7 +20,7 @@ int sum_them_all(const unsigned int n, ...)
 	}
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(list, int);
+		sum = sum + va_arg(list, int);
 	}
 	va_end(list);
 	return (sum);
