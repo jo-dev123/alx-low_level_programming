adcb739265209564b89ab09a5cd031e23bf02215
jo-dev123 <johnsondegbor@gmail.com>
2022-04-15 17:23:39 -0700

    adding 3-print_all.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,4 +9,16 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+ * struct fm -structure
+ * @fm: Pointer
+ * @p: pointer
+ */
+
+typedef struct fm
+{
+	char *fm;
+	void (*p)();
+} format_type;
+
 #endif
