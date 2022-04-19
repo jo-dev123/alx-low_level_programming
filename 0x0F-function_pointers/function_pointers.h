fc11cc29fa1b7f19119d26d24e56a19679cf7541
jo-dev123 <johnsondegbor@gmail.com>
2022-04-19 05:22:11 -0700

    adding stddef

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,9 @@
 #ifndef FUNCTION_POINTERS
 #define FUNCTION_POINTERS
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef>
 
 void print_name(char *name, void (*f)(char *));
 int int_index(int *array, int size, int (*cmp)(int));
