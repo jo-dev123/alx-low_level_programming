f282bcdeaaf1fbcec9338f4baf7d381d17fe7aec
jo-dev123 <johnsondegbor@gmail.com>
2022-05-24 11:18:48 -0700

    adding 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * jack_bauer - print out time by minute
+ * @void: no parameter
+ *
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+		}
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,3 +6,4 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 int _islower(int c);
+void jack_bauer(void);
