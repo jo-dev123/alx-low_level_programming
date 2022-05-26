fad49854cab6409cdca217e030010d0089d7af85
jo-dev123 <johnsondegbor@gmail.com>
2022-05-26 04:31:02 -0700

    adding 11-print_to_98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - print natural numbers from n-98
+ * @n: staerting number
+ *
+ * Return: void (no return value)
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (n = n; n <= 98; n++)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+		}
+	}
+	else
+	{
+		for (n = n; n >= 98; n--)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+		}	
+	}
+	printf("\n");
+}
