20199a39faa8a462d24ba52acaa930f5311db9c9
jo-dev123 <johnsondegbor@gmail.com>
2022-05-26 07:59:24 -0700

    adding 3-puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints string
+ * @str: string
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	for (*str = *str; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
