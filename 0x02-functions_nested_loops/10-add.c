7bcb2a570becf3baa96d5a22251552a1443fac0f
jo-dev123 <johnsondegbor@gmail.com>
2022-05-25 17:10:43 -0700

    pushing 10.add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adding two int
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: 0
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	_putchar(sum + '0');
+}
