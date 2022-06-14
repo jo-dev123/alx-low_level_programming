c5a7ebea8fdb7554a73ae6d19be175ed2ca4a086
jo-dev123 <johnsondegbor@gmail.com>
2022-06-13 19:14:19 -0700

    adding 100-operations.so

---
+++ 0x18-dynamic_libraries/c_func.c
@@ -0,0 +1,59 @@
+/**
+ * add - return addtion
+ * @a: first operand
+ * @b: second operand
+ * Return: result
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtraction
+ * @a: first operand
+ * @b: second operand
+ * Return: result
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * div - division
+ * @a: first operand
+ * @b: second operand
+ * Return: result
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mul - multiflication
+ * @a: first operand
+ * @b: second operand
+ * Return: result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * mod - modulo
+ * @a: first operand
+ * @b: second operand
+ * Return: result
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
