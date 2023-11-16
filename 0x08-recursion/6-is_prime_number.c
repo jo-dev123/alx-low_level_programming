4191af967997ce4bc5ea11e8c9ed5440b2c86ff8
jo-dev123 <johnsondegbor@gmail.com>
2023-11-15 19:32:11 -0800

    adding check when num==quot and removting quote as a typo

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -12,6 +12,8 @@ int compare(int num, int quot)
 {
 	if (num <= 1)
 		return (0);
+	else if (num == quot)
+		return (1);
 	else if (num % quot == 0)
 		return (0);
 	return (compare(num, quot + 1));
