5d71fdb2e3d312d9af4ceba1ab488fc35e021d65
jo-dev123 <johnsondegbor@gmail.com>
2023-11-17 18:12:45 -0800

    adding 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,28 +1,20 @@
+#include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - Entry Point
- * @argc: counts programs arguments
- * @argv: array of pointer to strings
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: array of strings
  *
- * Description: returns number of argument
- * Return: argc
+ * Return: EXIT_SUCCESS
  */
 
-int main(int argc, char *argv[])
+int main(int argc, __attribute__((unused)) char **argv)
 {
-	int i;
+	if (argc > 0)
+		printf("%d\n", argc - 1);
 
-	if (argc == 1)
-	{
-		printf("%d\n", 0);
-	}
-	else
-	{
-		for (i = 0; *argv; i++, argv++)
-			;
-		printf("%d\n", i - 1);
-	}
-
-	return (0);
+	return (EXIT_SUCCESS);
 }
+
